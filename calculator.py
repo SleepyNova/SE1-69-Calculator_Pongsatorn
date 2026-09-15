6198649f31dc3beb6e8b4fbdeeb04762cbe06a5f
SleepyNova <pongsatornstk@gmail.com>
2026-09-15 10:32:07 +0700

    update calculator.py

--- calculator.py
+++ calculator.py
@@ -20,10 +20,9 @@ def calculator():
   print("=== Python Calculator ===")
   print("1. Add (+)")
   print("2. Subtract (-)")
-  print("3. Multiply (*)")
-  print("4. Divide (/)")
 
-  choice = input("Select operation (1/2/3/4): ")
+
+  choice = input("Select operation (1/2/): ")
 
   if choice in ("1", "2", "3", "4"):
     try:
@@ -37,10 +36,7 @@ def calculator():
       print(f"Result: {num1} + {num2} = {add(num1, num2)}")
     elif choice == "2":
       print(f"Result: {num1} - {num2} = {subtract(num1, num2)}")
-    elif choice == "3":
-      print(f"Result: {num1} * {num2} = {multiply(num1, num2)}")
-    elif choice == "4":
-      print(f"Result: {num1} / {num2} = {divide(num1, num2)}")
+      
   else:
     print("Invalid Input")
 
